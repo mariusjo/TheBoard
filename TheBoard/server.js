953671c99ffaa108b155531e4cd0f480f1f19252
Marius <mariusjohans@gmail.com>
2015-10-23 10:10:31 +0200

    added

---
+++ TheBoard/server.js
@@ -0,0 +1,45 @@
+﻿var http = require('http');
+var express = require('express');
+
+var app = express();
+//support master pages
+//var ejsEngine = require('ejs-locals');
+
+
+//Setup the View Engine
+
+//jade
+//Express takes care of require, etc.
+//app.set("view engine", "jade");
+
+//EJS
+//usingEJS-locals ->  have to require ourselves.
+//app.engine("ejs", ejsEngine);
+//app.set("view engine", "ejs");
+
+//vash
+app.set("view engine", "vash");
+
+//defining server app routes
+app.get("/", function (req, res) {
+    res.render("index", {title:"Express + Vash"});
+});
+
+app.get("/api/users", function (req, res) {
+    res.set({
+        "Cache-Control": "no-cache, no-store, must-revalidate",
+        "rompe":"jejeje",
+        "Content-Type": "application/json"
+    });
+    //application/json  --  'text/plain'
+    //res.contentType('application/json');
+    //This will be serialized as JSON
+    res.send({ name: "Marius", isvalid: true, group: "Admin" });
+    //res.send("<user><name>Marius</name><isValid>true</isValid><group>Admin</group></user>");
+
+
+});
+
+var server = http.createServer(app);
+
+server.listen(3000);
