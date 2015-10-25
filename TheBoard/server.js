de0e4171a9ecc82b6a048964040eab65786deff7
Marius <mariusjohans@gmail.com>
2015-10-26 00:13:02 +0100

    finished data access with node.js

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -5,12 +5,24 @@ var app = express();
 //support master pages
 //var ejsEngine = require('ejs-locals');
 
+var controllers = require('./controllers');
+
+var bodyParser = require('body-parser');
+
+var flash = require('connect-flash');
+
+var session = require('express-session');
+
+var cookieParser = require('cookie-parser');
+
+var testing = null;
 
 //Setup the View Engine
 
 //jade
 //Express takes care of require, etc.
 //app.set("view engine", "jade");
+
 
 //EJS
 //usingEJS-locals ->  have to require ourselves.
@@ -20,22 +32,33 @@ var app = express();
 //vash
 app.set("view engine", "vash");
 
-//defining server app routes
-app.get("/", function (req, res) {
-    res.render("index", {title:"Express + Vash"});
-});
+//opt into services
+//needed in order to ge the body from a post
+app.use(bodyParser.urlencoded({extended: true}))
+//for temp. storing of info in session
+app.use(cookieParser());
+app.use(session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true}));
+app.use(flash());
+
+
+//set the public static resource folder
+app.use(express.static(__dirname+"\\public"));
+
+//init controllers
+controllers.init(app);
 
 app.get("/api/users", function (req, res) {
     res.set({
-        "Cache-Control": "no-cache, no-store, must-revalidate",
-        "rompe":"jejeje",
         "Content-Type": "application/json"
     });
     //application/json  --  'text/plain'
     //res.contentType('application/json');
     //This will be serialized as JSON
     res.send({ name: "Marius", isvalid: true, group: "Admin" });
-    //res.send("<user><name>Marius</name><isValid>true</isValid><group>Admin</group></user>");
+    
 
 
 });
