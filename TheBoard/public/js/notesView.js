6571533002d307a7f8fc86232cd176909afac704
Marius <mariusjohans@gmail.com>
2015-10-26 20:29:39 +0100

    Done with angular.js module

---
+++ TheBoard/public/js/notesView.js
@@ -0,0 +1,51 @@
+﻿//notesView.js
+(function (angular) {
+    var theModule = angular.module("notesView", ["ui.bootstrap"]);
+
+    theModule.controller("notesViewController", ["$scope", "$window", "$http", function ($scope, $windows, $http) {
+            $scope.notes = [];
+            $scope.newNote = createBlankNote();
+
+            //Get the category name
+            var urlParts = $windows.location.pathname.split("/");
+            var categoryName = urlParts[urlParts.length - 1];
+
+            var notesUrl = "/api/notes/" + categoryName;
+
+            $http.get(notesUrl).
+                then(function (result) {
+                    //success   
+                $scope.notes = result.data;
+                }, function (err) {
+                    //Error   
+                    //TODO
+                alert(err);
+            });
+
+            $scope.save = function () {
+                $http.post(notesUrl, $scope.newNote).
+                then(function (result) {
+                    //success   
+                    $scope.notes.push(result.data);
+                    $scope.newNote = createBlankNote();
+                }, function (err) {
+                    //Error   
+                    //TODO
+                    alert(err);
+                });
+            };
+
+            function createBlankNote() {
+                return {
+                    note: "",
+                    color: "yellow",
+                    author: ""
+                }
+            };
+            
+            
+
+    }]);
+
+
+})(window.angular);
